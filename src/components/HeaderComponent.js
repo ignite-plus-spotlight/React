f6fa1184c23e9f166d4590bcaf3fdbdf2e5db9da
rithikaprakash <60447231+rithikaprakash@users.noreply.github.com>
2020-07-12 00:19:18 +0530

    Header,Footer and Carousel Added

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,27 +2,21 @@ import React, {Component} from 'react';
 import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
     Button, Modal, ModalHeader, ModalBody,
     Form, FormGroup, Input, Label } from 'reactstrap';import {NavLink} from 'react-router-dom';
-import Logo from '../assets/images/logo.png'
+
 
 class Header extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            isNavOpen: false,
             isModalOpen: false
         };
-        this.toggleNav=this.toggleNav.bind(this);
+        
         this.toggleModal = this.toggleModal.bind(this);
         this.handleLogin = this.handleLogin.bind(this);
 
     }
 
-    toggleNav() {
-        this.setState({
-            isNavOpen:!this.state.isNavOpen
-        });
-    }
     toggleModal() {
         this.setState({
           isModalOpen: !this.state.isModalOpen
@@ -39,54 +33,21 @@ class Header extends Component {
     render() {
         return(
             <>
-                <Navbar dark expand="md">
+                <Navbar className="navbar-light"  expand="md">
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav}/>
                         <NavbarBrand className="mr-auto" href="/">
-                            <img src={Logo} height="30" width="41"
-                            alt="Spotlight"/>
+                            {/* <img src={Logo} height="50" width="100"
+                            alt="Spotlight"/> */} 
+                            <h3>SPOTLIGHT</h3>
                         </NavbarBrand>
-                        <Collapse isOpen={this.state.isNavOpen} navbar>
-                        <Nav navbar>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/home">
-                                    <span className="fa fa-home fa-lg"></span>Home
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/aboutus">
-                                    <span className="fa fa-info fa-lg"></span>About Us
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/menu">
-                                    <span className="fa fa-list fa-lg"></span>Menu
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/contactus">
-                                    <span className="fa fa-address-card fa-lg"></span>Contact Us
-                                </NavLink>
-                            </NavItem>
-                        </Nav>
                         <Nav className="ml-auto" navbar>
                                 <NavItem>
                                     <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
                                 </NavItem>
                             </Nav>
-                        </Collapse>
                     </div>
                 </Navbar>
-                
-                <Jumbotron>
-                    <div className="container">
-                        <div className="row row-header">
-                            <div className="col-12 col-sm-6">
-                                <h1>Employee Recognition Platform</h1>
-                            </div>
-                        </div>
-                    </div>
-                </Jumbotron>
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
                     <ModalBody>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,15 @@
 import React,{Component} from 'react';
 import Header from './HeaderComponent';
+import Sliding from './SlidingComponent';
 import Footer from './FooterComponent';
+
+
  class Main extends Component {
     render() {
         return (
             <div>
                 <Header/>
+                <Sliding/>
                 <Footer/>
             </div>
         );
